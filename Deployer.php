8ae55d6fb8b34d152acfcea7a47c8ff58e903ca8
Gonzalo Payo <gpayon@gmail.com>
2016-06-11 10:40:13 +0200

    More changes

--- Deployer.php
+++ Deployer.php
@@ -25,7 +25,6 @@ class Deployer extends Module implements BootstrapInterface {
         if ($app instanceof \yii\console\Application) {
             $app->controllerMap[$this->id] = [
                 'class' => 'gpayo\deployer\commands\DeployerController',
-                'module' => $this,
             ];
         }
     }
